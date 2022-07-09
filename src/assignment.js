269a94d1cf8028bebdd565e4984ae33031c44cbd
Chukwuebuka Obi <90873641+EbukaObi@users.noreply.github.com>
2022-07-09 16:48:26 +0000

    Assignment challange 1 and 2 done

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,17 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+
+    for (i=0; i<arrayOfNumbers.length; i++){
+        sum = sum + arrayOfNumbers[i];
+    }
+
+    return sum
+
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +36,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let count = 0;
+    for (i=0; i<arrayOfNumbers.length; i++){
+        if (arrayOfNumbers[i] % 2 == 0){
+            count++
+        }
+    }
+
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,6 +62,8 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
+
+
 function celsiusToFahrenheit(arrayOfNumbers) {
     return arrayOfNumbers;
 }
